3f542585f7625afc4bf0ae95e71ab7a020a7b3f3
Rafael de Carvalho <rafaeldecarvalho.ps@gmail.com>
2020-08-21 10:09:00 -0300

    chore: add aws-amplify config

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "sa-east-1",
+    BUCKET: "serverless-notes-tuto"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://1r4edadkzg.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_cAjXLKmQu&#39",
+    APP_CLIENT_ID: "2lgnck3jamhc1dkp6q8a95ndf",
+    IDENTITY_POOL_ID: "us-east-2:060e8b3a-0548-4859-adac-225f64509a07&#39"
+  }
+};
